9cc941c6e746315d687240cc1da425d105dd8720
Pari <pari.thanponsri@gmail.com>
2025-10-08 21:01:48 +0700

    Add files via upload

---
+++ ENGCC304/Homework/lab5.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char employeeID[10]; 
+    int workingHours;
+    float salaryPerHour;
+    float totalSalary;
+
+    printf("Input the Employees ID(Max. 10 chars): ");
+    scanf("%10s", employeeID);
+
+    printf("Input the working hrs: ");
+    scanf("%d", &workingHours);
+
+    printf("Salary amount/hr: ");
+    scanf("%f", &salaryPerHour);
+
+    totalSalary = workingHours * salaryPerHour;
+
+    printf("----\n");
+    printf("Expected Output:\n");
+    printf("Employees ID = %s\n", employeeID);
+    printf("Salary = U$ %.2f\n", totalSalary);
+
+    return 0;
+}
