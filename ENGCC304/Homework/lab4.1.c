20d542d2bb548fc481b54f39ada11ab1a2093baf
Pari <pari.thanponsri@gmail.com>
2025-10-12 17:05:23 +0700

    Update lab4.1.c

--- ENGCC304/Homework/lab4.1.c
+++ ENGCC304/Homework/lab4.1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int days;
-    long seconds;
+    int d;
+    long s;
 
     printf("Input Days : ");
-    scanf("%d", &days);
+    scanf("%d", &d);
 
-    seconds = days * 24 * 60 * 60;
+    s = d * 86400;
 
-    printf("%d days = %ld seconds\n", days, seconds);
+    printf("%d days = %ld seconds\n", d, s);
 
     return 0;
 }
