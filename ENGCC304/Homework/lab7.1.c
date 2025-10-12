dd20f98d252944fdb4f6db10155013c98e90d594
Pari <pari.thanponsri@gmail.com>
2025-10-12 20:25:33 +0700

    Create lab7.1.c

---
+++ ENGCC304/Homework/lab7.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    printf("Enter value:\n");
+    scanf("%d", &N);
+
+    printf("Series: ");
+
+    if (N % 2 == 1) {
+        // กรณีเลขคี่: แสดงเลขคี่จาก 1 ถึง N
+        for (int i = 1; i <= N; i += 2) {
+            printf("%d ", i);
+        }
+    } else {
+        // กรณีเลขคู่: แสดงเลขคู่จาก N ถึง 0
+        for (int i = N; i >= 0; i -= 2) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
